d254c3d6abf04a1b010a1da3e09a573a6f90c2ec
myhauntedspoon <drewhinckley2009@gmail.com>
2024-10-22 09:02:16 -0500

    optimize code

--- dnddice.py
+++ dnddice.py
@@ -1,60 +1,55 @@
 import random
 import time
 
-def roll_dice(num):
-    return random.randint(1, num)
+def continue_():
+    input("\npress enter to continue...\n")
 
 def dice_number(dice):
         time.sleep(0.5)
         print('')
-        print(f'Your roll is... {roll_dice(dice)}!')
+        print(f'Your roll is... {random.randint(1, dice)}!')
         print('')
         time.sleep(0.5)
 
-def dice_select():
-    print("Select one of the following dice to roll:")
-    print('')
-    time.sleep(1)
-    print('[1] - D4')
-    time.sleep(0.1)
-    print('[2] - D6')
-    time.sleep(0.1)
-    print('[3] - D8')
-    time.sleep(0.1)
-    print('[4] - D10')
-    time.sleep(0.1)
-    print('[5] - D12')
-    time.sleep(0.1)
-    print('[6] - D20')
-    time.sleep(0.1)
-    print('')
-    print('[7] - Quit')
-    time.sleep(1)
-    print('')
-    print('Which dice would you like to roll?')
-    choose_dice = input('>> ')
+def select_dice():
+    
+    dice = [
+        'D4',
+        'D6',
+        'D8',
+        'D10',
+        'D12',
+        'D20'
+    ]
 
-    if choose_dice.lower().strip() in ('1', 'd4'):
-        dice_number(4)
-    elif choose_dice.lower().strip() in ('2', 'd6'):
-        dice_number(6)
-    elif choose_dice.lower().strip() in ('3', 'd8'):
-        dice_number(8)
-    elif choose_dice.lower().strip() in ('4', 'd10'):   
-        dice_number(10)
-    elif choose_dice.lower().strip() in ('5', 'd12'):
-        dice_number(12)
-    elif choose_dice.lower().strip() in ('6', 'd20'):
-        dice_number(20)
-    elif choose_dice.lower().strip() in ('7', 'quit'):
-        return game_active == False
+    dice_options = {
+        '1': 4, 'd4': 4,
+        '2': 6, 'd6': 6,
+        '3': 8, 'd8': 8,
+        '4': 10, 'd10': 10,
+        '5': 12, 'd12': 12,
+        '6': 20, 'd20': 20,
+        '7': 'quit', 'quit': 'quit'
+    }
+
+    print("Select one of the following dice to roll:\n")
+    for i, die in enumerate(dice, start=1):
+        print(f'[{i}] - {die}')
+    print("[7] - quit")
+
+    choose_dice = input('\nWhich dice would you like to roll?\n>> ')
+    answer = choose_dice.lower().strip()
+
+    if answer in dice_options:
+        if dice_options[answer] == 'quit':
+             return False
+        dice_number(dice_options[answer])
+        continue_()
+        return True
     else:
-        print(f'"{choose_dice}" is not a valid choice.')
-        return game_active == True
+        print(f'{choose_dice} is not a valid die.')
 
-    return game_active == True
-
-if __name__ == "__main__":
-    game_active = True
-    while game_active:
-        game_active = dice_select()
+if __name__ == '__main__':
+     game_active = True
+     while game_active:
+          game_active = select_dice()
